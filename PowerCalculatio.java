a2d64a74da10cc81e46b12e17776c53b51692794
dibbi-chakraborty <dibbichakraborty@gmail.com>
2025-03-20 21:19:57 +0600

    Create PowerCalculatio.java

---
+++ PowerCalculatio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PowerCalculation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter base: ");
+        int base = sc.nextInt();
+        System.out.print("Enter exponent: ");
+        int exponent = sc.nextInt();
+
+        long result = 1;
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+        System.out.println(base + " raised to the power " + exponent + " is: " + result);
+    }
+}
